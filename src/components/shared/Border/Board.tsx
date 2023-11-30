321ecb770c9b8c36cf4dd238e45b2a91141ff243
Crewch <artemkr2003@mail.ru>
2023-11-30 20:29:01 +0300

    rework

--- src/components/shared/Border/Board.tsx
+++ src/components/shared/Border/Board.tsx
@@ -1,6 +1,4 @@
-'use client'
-
-import { FC, useEffect, useState } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 import styles from './Board.module.scss'
 import { Socket } from 'socket.io-client'
 
@@ -10,109 +8,96 @@ interface BoardProps {
 }
 
 const Board: FC<BoardProps> = ({ color, socket }) => {
+	const canvasRef = useRef<HTMLCanvasElement>(null)
+	const contextRef = useRef<CanvasRenderingContext2D | null>(null)
 	const [boardData, setBoardData] = useState<string>()
 
-	let timeout: { data: any } = { data: '' }
-
 	const drawOnCanvas = () => {
-		let canvas = document.querySelector('#board') as HTMLCanvasElement
+		const canvas = canvasRef.current as HTMLCanvasElement
+		const context = canvas.getContext('2d') as CanvasRenderingContext2D
+		contextRef.current = context
 
-		let ctx = canvas.getContext('2d') as CanvasRenderingContext2D
-
-		let sketch = document.querySelector('#sketch') as HTMLCanvasElement
-
-		let sketch_style = getComputedStyle(sketch)
-		canvas.width = parseInt(sketch_style.getPropertyValue('width'))
-
-		canvas.height = parseInt(sketch_style.getPropertyValue('height'))
-
-		let mouse = { x: 0, y: 0 }
-		let last_mouse = { x: 0, y: 0 }
-
-		canvas.addEventListener(
-			'mousemove',
-			function (e) {
-				last_mouse.x = mouse.x
-				last_mouse.y = mouse.y
-
-				mouse.x = e.pageX - this.offsetLeft
-				mouse.y = e.pageY - this.offsetTop
-			},
-			false
-		)
-
-		ctx.lineWidth = 5
-		ctx.lineJoin = 'round'
-		ctx.lineCap = 'round'
-		ctx.strokeStyle = color
-
-		let onPaint = function () {
-			ctx.beginPath()
-			ctx.moveTo(last_mouse.x, last_mouse.y)
-			ctx.lineTo(mouse.x, mouse.y)
-			ctx.closePath()
-			ctx.stroke()
-
-			if (timeout.data != undefined) {
-				clearTimeout(timeout.data)
-			}
-
-			timeout.data = setTimeout(() => {
-				setBoardData(canvas.toDataURL('image/png'))
-			}, 1000)
+		if (!canvas || !context) {
+			return
 		}
 
-		canvas.addEventListener(
-			'mousedown',
-			function (e) {
-				canvas.addEventListener('mousemove', onPaint, false)
-			},
-			false
-		)
+		const sketch = document.querySelector('#sketch')
+		const sketchStyle = getComputedStyle(sketch as Element)
+		canvas.width = parseInt(sketchStyle.getPropertyValue('width'))
+		canvas.height = parseInt(sketchStyle.getPropertyValue('height'))
 
-		canvas.addEventListener(
-			'mouseup',
-			function () {
-				canvas.removeEventListener('mousemove', onPaint, false)
-			},
-			false
-		)
+		const mouse = { x: 0, y: 0 }
+		const lastMouse = { x: 0, y: 0 }
+
+		const mouseMoveHandler = (e: MouseEvent) => {
+			lastMouse.x = mouse.x
+			lastMouse.y = mouse.y
+
+			mouse.x = e.pageX - canvas.offsetLeft
+			mouse.y = e.pageY - canvas.offsetTop
+		}
+
+		canvas.addEventListener('mousemove', mouseMoveHandler, false)
+
+		context.lineWidth = 5
+		context.lineJoin = 'round'
+		context.lineCap = 'round'
+		context.strokeStyle = color
+
+		const onPaint = () => {
+			context.beginPath()
+			context.moveTo(lastMouse.x, lastMouse.y)
+			context.lineTo(mouse.x, mouse.y)
+			context.closePath()
+			context.stroke()
+
+			setTimeout(() => setBoardData(canvas.toDataURL('image/png')), 1000)
+		}
+
+		const startDrawing = (e: MouseEvent) => {
+			canvas.addEventListener('mousemove', onPaint, false)
+		}
+
+		const stopDrawing = () => {
+			canvas.removeEventListener('mousemove', onPaint, false)
+		}
+
+		canvas.addEventListener('mousedown', startDrawing, false)
+		canvas.addEventListener('mouseup', stopDrawing, false)
 	}
 
-	useEffect(() => {
-		drawOnCanvas()
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	useEffect(drawOnCanvas, [])
 
 	useEffect(() => {
-		const canvas = document.querySelector('#board') as HTMLCanvasElement
-
-		if (canvas) {
-			const ctx = canvas.getContext('2d')
-
-			if (ctx) {
-				ctx.strokeStyle = color
-			}
+		const context = contextRef.current
+		if (context) {
+			context.strokeStyle = color
 		}
 	}, [color])
 
 	useEffect(() => {
-		socket.emit('canvas-data', { boardData })
+		socket.emit('canvas-data', boardData)
 	}, [boardData, socket])
 
 	useEffect(() => {
-		socket.on('canvas-data', data => {
+		socket.on('canvas-data', (data: string) => {
 			const image = new Image()
-			const canvas = document.querySelector('#board') as HTMLCanvasElement
-			let ctx = canvas.getContext('2d') as CanvasRenderingContext2D
-			image.onload = () => ctx.drawImage(image, 0, 0)
+			const canvas = canvasRef.current
+			const context = contextRef.current
+
+			if (!canvas || !context) {
+				return
+			}
+
+			image.onload = () => context.drawImage(image, 0, 0)
 			image.src = data
 		})
 	}, [socket])
 
 	return (
 		<div className={styles.sketch} id='sketch'>
-			<canvas className={styles.board} id='board' />
+			<canvas ref={canvasRef} className={styles.board} id='board' />
 		</div>
 	)
 }
